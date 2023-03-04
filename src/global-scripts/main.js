6adef3b777be18a166ddc55a32a670a2a84acaeb
Wojciech Pająk <arachnomorph@gmail.com>
2023-03-04 14:03:10 +0100

    cleanup code, improve clarity

--- src/global-scripts/main.js
+++ src/global-scripts/main.js
@@ -1,5 +1,1 @@
-/// Main JavaScript File
-/// Here we import all the global JavaScript files we need for our project.
-
 import '../global-styles/style.scss'
-
--- src/pages/home-page/scripts/home-page.js
+++ src/pages/home-page/scripts/home-page.js
@@ -1,4 +1,1 @@
-/// Homepage JavaScript File
-/// Here we import all the JavaScript files we need for our homepage.
-
 import '../styles/home-page.scss'
